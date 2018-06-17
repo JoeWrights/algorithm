78881058c99dbf10516f93d7771d76fc9bbc55ac
JoeWrights <1035208578@qq.com>
2018-06-17 23:47:20 +0800

    add bubble sort

--- sort/bubble.js
+++ sort/bubble.js
@@ -15,3 +15,24 @@ function bubbleSort(arr) {
     return arr;
 }
 console.log(bubbleSort([3, 2, 6, 7, 5, 9, 10]));
+
+//改良版本
+function bubbleSort2(arr){
+    console.time("耗时");
+    var i=arr.length-1;
+    while(j<i){
+        var pos=0;
+        for(var j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                pos=j;
+                var temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+        i=pos;
+    }
+    console.timeEnd("耗时");
+    return arr;
+}
+console.log(bubbleSort2([3, 2, 6, 7, 5, 9, 10]));
